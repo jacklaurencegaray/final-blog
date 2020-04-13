3194e56b85ce95f162582b65e41b4ff11bab6069
Jack Laurence <lawrencetecho@gmail.com>
2020-04-13 15:30:14 +0800

    Updated config

--- config/website.js
+++ config/website.js
@@ -1,28 +1,28 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
-  siteTitle: 'My Blog', // Navigation and Site Title
-  siteTitleAlt: 'The blog of an egghead creator', // Alternative Site title for SEO
-  siteTitleShort: 'Starter Blog', // short_name for manifest
-  siteUrl: 'https://your-site.io', // Domain of your site. No trailing slash!
+  siteTitle: 'Codecreamer', // Navigation and Site Title
+  siteTitleAlt: 'A Personal Blog by Jack', // Alternative Site title for SEO
+  siteTitleShort: 'A Personal Blog', // short_name for manifest
+  siteUrl: 'https://blog.jacklaurence.net', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
-  siteDescription: 'This is where they post things!',
+  siteDescription:
+    'A blog about mostly about code and everything I find interesting.',
   author: 'Author', // Author for schemaORGJSONLD
-  organization: 'egghead.io LLC',
+  organization: 'Jack Laurence',
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@eggheadio', // Twitter Username
-  ogSiteName: 'egghead.io Gatsby Starter Blog', // Facebook Site Name
+  userTwitter: '@lawrencetecho', // Twitter Username
   ogLanguage: 'en_US',
   googleAnalyticsID: '',
 
   // Manifest and Progress color
-  themeColor: '#5348FF',
+  themeColor: '#24292f',
   backgroundColor: '#2b2e3c',
 
   // Social component
-  twitter: 'https://twitter.com/eggheadio/',
-  twitterHandle: '@eggheadio',
-  github: 'https://github.com/eggheadio/',
-  linkedin: '',
+  twitter: 'https://twitter.com/lawrencetecho',
+  twitterHandle: '@lawrencetecho',
+  github: 'https://github.com/jacklaurencegaray',
+  linkedin: 'https://linkedin.com/in/jacklaurence',
 }
