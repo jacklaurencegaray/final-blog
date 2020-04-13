66035ce2b85b5932fed3ad9413bab823db7a04ba
Jack Garay <lawrencetecho@gmail.com>
2020-04-13 17:00:26 +0800

    Keywords

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     title: config.siteTitle,
     twitterHandle: config.twitterHandle,
     description: config.siteDescription,
-    keywords: ['Video Blogger'],
+    keywords: ['personal blog', 'blog', 'code', 'javascript', 'react'],
     canonicalUrl: config.siteUrl,
     image: config.siteLogo,
     author: {
